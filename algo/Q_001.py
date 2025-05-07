3dc154bb7caa11fe4fd00708fc6d5e4ba765df45
cdtong1004 <cdtong1004@naver.com>
2025-05-07 15:27:38 +0900

    Q_001

---
+++ algo/Q_001.py
@@ -0,0 +1,24 @@
+# 입력 S , N 
+
+# 출력 예
+# cat 2 --> aacat
+# 
+# 1 <= N <= 100
+
+# 입력값 설정 
+S = input()
+N = int(input())
+
+enhance = 'a'
+
+if N >= 0 and N <= 100:
+    print(f'{enhance*N}{S}')
+        
+
+
+
+
+
+
+
+
