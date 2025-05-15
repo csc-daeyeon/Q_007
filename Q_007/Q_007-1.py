9b0cb42ce9e86e973649e75aa58042945b394392
KIMDAEYEON <daeyeon.kim@cscloud.co.jp>
2025-05-15 11:06:20 +0900

    first

---
+++ Q_007/Q_007-1.py
@@ -0,0 +1,24 @@
+# N : 사용자 입력 값 
+# X : 버그 입력 값 
+# 입력값안에 버그의 원인이 되는 입력값 갯수 출력 
+
+A = []
+cnt = 0
+
+N, X = map(int, input().split())
+
+X_str = str(X)
+
+A = list(map(str, input().split()))
+
+#for i in A:
+#    if X_str in i:
+#        cnt += 1
+
+for i in A:
+    print(i)
+
+print(cnt)
+
+
+
